2039bc38ad635137a8be96304d8d1ddc413ed194
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-07-23 00:42:11 +0800

    search rotated array

--- group/SearchinRotatedSortedArray_33.java
+++ group/SearchinRotatedSortedArray_33.java
@@ -2,6 +2,19 @@ package wechat.group;
 
 public class SearchinRotatedSortedArray_33 {
     public int search(int[] nums, int target) {
-
+        if (nums.length == 0) return -1;
+        int lo = 0, hi = nums.length -1;
+        while (lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < nums[hi]){
+                if (nums[mid] < target && nums[hi] >= target) lo = mid + 1;
+                else hi = mid - 1;
+            }else{
+                if (nums[mid] > target && nums[lo] <= target) hi = mid - 1;
+                else lo = mid + 1;
+            }
+        }
+        return -1;
     }
 }
