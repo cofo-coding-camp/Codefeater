6f87798b4a3abbd4f17aeef858e21e960cebf70c
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-07-31 09:08:50 +0800

    square make

---
+++ group/SquareMake_473.java
@@ -0,0 +1,36 @@
+package wechat.group;
+
+import java.util.Arrays;
+
+public class SquareMake_473 {
+    public boolean makesquare(int[] nums) {
+        if (nums == null || nums.length < 4) return false;
+        int sum = 0;
+        for (int i : nums) sum += i;
+        if (sum % 4 != 0) return false;
+        Arrays.sort(nums);
+        boolean[] visited = new boolean[nums.length];
+        return edge_judge(nums, visited, sum/4);
+    }
+
+    public boolean edge_judge(int[] nums, boolean[] visited, int target){
+        for (int i = 0; i < 4; i ++){
+            if(!dfs(nums, visited, target)) return false;
+        }
+        return true;
+    }
+
+    public boolean dfs(int[] nums, boolean[] visited, int target){
+        if (target == 0) return true;
+        for (int i = nums.length - 1; i >= 0; i ++){
+            int chop = nums[i];
+            if (!visited[i] && target >= chop){
+                visited[i] = true;
+                if (dfs(nums, visited, target - chop)) return true;
+                visited[i] = false;
+
+            }
+        }
+        return false;
+    }
+}
