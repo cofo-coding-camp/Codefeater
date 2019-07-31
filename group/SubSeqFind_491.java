d1e2d9542e722b419e6ec07fd128474a36080d0d
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-07-31 20:51:06 +0800

    findSubsequences

--- group/SubSeqFind_491.java
+++ group/SubSeqFind_491.java
@@ -1,29 +1,31 @@
 package wechat.group;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+
 public class SubSeqFind_491 {
+
+    Set<List<Integer>> set = new HashSet<>();
     public List<List<Integer>> findSubsequences(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        List<Integer> aux = new ArrayList<>();
         Arrays.sort(nums);
-        boolean[] visited = new boolean[nums.length];
-        dfs(nums, res, aux, 0, visited);
-        return res;
+        if (nums.length >= 2) {
+            for (int i = 0; i < nums.length - 1; i ++){
+                List<Integer> aux = new ArrayList<>();
+                aux.add(nums[i]);
+                dfs(nums, aux , i + 1);
+            }
+        }
+        return new ArrayList<>(set);
     }
 
-    public void dfs(int[] nums, List<List<Integer>> res, List<Integer> aux, int idx, boolean[] visited){
-        if (aux.size() >= 2) {
-            res.add(new ArrayList<>(aux));
-        }
+    public void dfs(int[] nums, List<Integer> aux, int idx){
+        if (aux.size() >= 2) set.add(new ArrayList<>(aux));
+        int last_tail = aux.get(aux.size() - 1);
+
         for (int i = idx; i < nums.length; i ++){
-            if (!visited[idx]){
-                visited[idx] = true;
-                aux.add(nums[idx]);
-                dfs(nums, res, aux, idx + 1, visited);
+            if (nums[i] >= last_tail){
+                aux.add(nums[i]);
+                dfs(nums, aux, i + 1);
                 aux.remove(aux.size() - 1);
-                visited[idx] = false;
             }
         }
         return;
