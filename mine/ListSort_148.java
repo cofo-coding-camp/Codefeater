6a237337798bd2b4ac9b60d6f49123be872437ed
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-08-27 23:32:53 +0800

    refix a bug : recursive is not a bug

--- mine/ListSort_148.java
+++ mine/ListSort_148.java
@@ -19,7 +19,7 @@ public class ListSort_148 {
     public ListNode merge(ListNode l1, ListNode l2){
         ListNode dummyNode = new ListNode(0);
         ListNode p = dummyNode;
-        while (l1 != null || l2 != null){
+        while (l1 != null && l2 != null){
             if (l1.val < l2.val){
                 p.next = l1;
                 l1 = l1.next;
