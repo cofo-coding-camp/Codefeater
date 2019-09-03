5a7876cda10b32a2b6f841a9450a78be581ba730
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-09-03 08:58:13 +0800

    typo rebug

--- mine/ContainerWithMostWater_11.java
+++ mine/ContainerWithMostWater_11.java
@@ -5,7 +5,7 @@ public class ContainerWithMostWater_11 {
         int maxArea = 0, l = 0, r = height.length - 1;
 
         while (l < r){
-            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - 1));
+            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - l));
 
             if (height[l] < height[r]) l ++;
             else r -- ;
