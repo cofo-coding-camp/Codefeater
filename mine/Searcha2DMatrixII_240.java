4b51cb3e6dccec472d5b719396a27d26f87755d0
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-09-04 23:26:15 +0800

    add a simple answer

--- mine/Searcha2DMatrixII_240.java
+++ mine/Searcha2DMatrixII_240.java
@@ -33,4 +33,16 @@ public class Searcha2DMatrixII_240 {
         }
         return false;
     }
+
+    public boolean searchMatrix_II(int[][] matrix, int target){
+        int row = matrix.length - 1;
+        int col = 0;
+
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] > target) row --;
+            else if (matrix[row][col] < target) col ++;
+            else return true;
+        }
+        return false;
+    }
 }
