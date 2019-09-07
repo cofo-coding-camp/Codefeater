c95962a2a56ad5555f5cb95178fef33767bca452
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-09-07 22:18:42 +0800

    set zeros

---
+++ mine/SetMatrixZeroes_73.java
@@ -0,0 +1,24 @@
+package wechat.mine;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMatrixZeroes_73 {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> row_zero = new HashSet<>();
+        Set<Integer> col_zero = new HashSet<>();
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        for (int i = 0; i < row; i ++)
+            for (int j = 0; j < col; j ++)
+                if (matrix[i][j] == 0) {
+                    row_zero.add(i);
+                    col_zero.add(j);
+                }
+
+        for (int i = 0; i < row; i ++)
+            for (int j = 0; j < col; j ++)
+                if (row_zero.contains(i) || col_zero.contains(j)) matrix[i][j] = 0;
+    }
+}
