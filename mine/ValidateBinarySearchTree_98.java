5fd45df6890ce066f2b2d3c3a054422c97445c2f
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-09-10 10:08:10 +0800

    add a recursive method

--- mine/ValidateBinarySearchTree_98.java
+++ mine/ValidateBinarySearchTree_98.java
@@ -2,7 +2,7 @@ package wechat.mine;
 
 public class ValidateBinarySearchTree_98 {
 
-    // recursive
+    // recursive 1
     public boolean isValidBST(TreeNode root) {
         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
@@ -11,6 +11,23 @@ public class ValidateBinarySearchTree_98 {
         if (root == null) return true;
         if (root.val >= maxVal || root.val <= minVal) return false;
         return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+
+    // recursive 2
+    public boolean helper(TreeNode node, Integer lower, Integer upper){
+        if (node == null) return true;
+
+        int val = node.val;
+        if (lower != null && val <= lower) return false;
+        if (upper != null && val >= upper) return false;
+
+        if (!helper(node.right, val, upper)) return false;
+        if (!helper(node.left, lower, val)) return false;
+        return true;
+    }
+
+    public boolean isValidBst_2(TreeNode root) {
+        return helper(root, null, null);
     }
 
     //iterative
