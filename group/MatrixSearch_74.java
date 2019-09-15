809bcab42b0a30a44f83bd8ceb8e295e0feb519b
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-09-15 19:50:41 +0800

    add log(mn) = logm + log n

--- group/MatrixSearch_74.java
+++ group/MatrixSearch_74.java
@@ -13,4 +13,24 @@ public class MatrixSearch_74 {
         }
         return false;
     }
+
+    public boolean searchMatrix_II(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
+
+        int left = 0, right = m *n - 1;
+        int pivotIdx, pivotEle;
+
+        while (left <= right) {
+            pivotIdx = (left + right)/2;
+            pivotEle = matrix[pivotIdx/n][pivotIdx % n];
+            if (target == pivotEle) return true;
+            else {
+                if (target < pivotEle) right = pivotIdx - 1;
+                else left = pivotIdx + 1;
+            }
+        }
+        return false;
+    }
 }
