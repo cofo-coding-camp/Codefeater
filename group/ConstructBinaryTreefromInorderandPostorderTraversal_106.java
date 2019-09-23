19bfa592851b6096ab3e740365a438efcef2b60c
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-09-24 00:19:22 +0800

    postorder and preorder

--- group/ConstructBinaryTreefromInorderandPostorderTraversal_106.java
+++ group/ConstructBinaryTreefromInorderandPostorderTraversal_106.java
@@ -1,10 +1,22 @@
 package wechat.group;
 
+import java.util.HashMap;
+
 public class ConstructBinaryTreefromInorderandPostorderTraversal_106 {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return helper(postorder, inorder, 0, inorder.length - 1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) map.put(inorder[i], i);
+        return helper(inorder, 0, inorder.length, postorder, 0, postorder.length, map);
     }
-    public TreeNode helper(int[] postorder, int[] inorder, int left, int right){
-        if (pos)
+
+    private TreeNode helper(int[] inorder, int i_start, int i_end, int[] postorder, int p_start, int p_end, HashMap<Integer, Integer> map){
+        if (p_start == p_end) return null;
+        int root_val = postorder[p_end - 1];
+        TreeNode root = new TreeNode(root_val);
+        int i_root_index = map.get(root_val);
+        int leftNum = i_root_index - i_start;
+        root.left = helper(inorder, i_start, i_root_index, postorder, p_start, p_start + leftNum, map);
+        root.right = helper(inorder, i_root_index + 1, i_end, postorder, p_start + leftNum, p_end - 1, map);
+        return root;
     }
 }
--- group/ConstructBinaryTreefromPreorderandInorderTraversal_105.java
+++ group/ConstructBinaryTreefromPreorderandInorderTraversal_105.java
@@ -12,6 +12,13 @@ public class ConstructBinaryTreefromPreorderandInorderTraversal_105 {
 
     //最大的想法是：将数组拆成2部分，然后再每个部分递归调用函数，完成数组的递归
     private TreeNode helper(int[] preorder, int p_start, int p_end, int[] inorder, int i_start, int i_end, HashMap<Integer, Integer> map){
-
+        if (p_start == p_end) return null;
+        int root_val = preorder[p_start];
+        TreeNode root = new TreeNode(root_val);
+        int i_root_index = map.get(root_val);
+        int leftNum = i_root_index - i_start;
+        root.left = helper(preorder, p_start + 1, p_start + leftNum + 1, inorder, i_start, i_root_index, map);
+        root.right = helper(preorder, p_start + leftNum + 1, p_end, inorder, i_root_index + 1, i_end, map);
+        return root;
     }
 }
